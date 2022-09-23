190803dffd82456476dac7b5bc56abc5ace8282e
paul serafim <paul.serafim.3021@gmail.com>
2022-09-23 14:44:17 +1000

    code

--- src/pages/MovieInfo.jsx
+++ src/pages/MovieInfo.jsx
@@ -10,70 +10,57 @@ import spiderman from '../assets/spiderman.jpg'
 function MovieInfo() {
     const { id } = useParams()
     const [movie, setMovie] = useState([])
-    const [movieid, setmovieid] = useState()
+  
+
+  async function getMovieDetails(id) {
+    const { data } = await axios.get(`https://api.themoviedb.org/3/movie/${id}api_key=c5339e7f37bc49952c323306fac80cff&language=en-US`)
+    setMovie(data)
+    console.log(data);
+  }
+
+  useEffect(() => {
+    getMovieDetails(id)
+  },[])
+  
  
-    async function getmovieId(id) {
-        const data = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=c5339e7f37bc49952c323306fac80cff&language=en-US`)
-        setmovieid(data)
-        console.log(data.data.results)
-    }
-    getmovieId()
     
-    useEffect(() => {
-        getmovieId(id)
-    }, [])
-
-
-
-
-
+    
   return (
-    <div className='movie__container'>
-        <div className="movies__row">
-            <div className="movie__back">
-                <Link to="/movies">
-                    <Button><ArrowBackIcon />Back</Button>
-                </Link>
-            </div>
+    <div className="movie__container">
+      <div className="movies__row">
+        <div className="movie__back">
+          <Link to="/movies">
+            <Button>
+              <ArrowBackIcon />
+              Back
+            </Button>
+          </Link>
         </div>
-        <div className="movie__selected">
-            <figure className='movie__selected--figure'>
-                <img src={spiderman} className='movie__selected--img' alt="" />
+      </div>
+          <div className="movie__selected">
+            <figure className="movie__selected--figure">
+              <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} className="movie__selected--img" alt="" />
             </figure>
-            
-            <div className="movie__selected--description">
-                {/* {movie.map((movie, i) => <div>{id}</div>)} */}
-                <h2 className="movie__selected--title">
-                    Spiderman
-                </h2>
-                <div className='movie__rating'>
-                    <img src="" alt="" />
-                    <h2>7.0</h2>
-                </div>
-                <h3 className="movie__selected--year">
-                    <b>Release Date</b>
-                    "2002"
-                </h3>
-                <h4 className="movie__selected--length">
-                    <b>Runtime</b>
-                    "90 mins"
-                </h4>
-                <h5 className="movie__selected--director">
-                    <b>Name:</b>
-                    "Spillburg"
-                </h5>
-                <h5 className="movie__selected--cast">
-                    <b>Actors:</b>
-                    ""
-                </h5>
-                <div className='summary'>
-                    <b>Summary</b>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus dolores pariatur quos adipisci sunt fugiat vel, corrupti sint reiciendis repellendus, ad, vitae distinctio unde magnam iure porro mollitia nihil asperiores.</p>
-                </div>
+            <div className="movie__selected--description" >
+              <h2 className="movie__selected--title">{movie.title}</h2>
+              <h3 className="movie__selected--year">
+                <b>Release Date:</b>
+                {movie.release_date}
+              </h3>
+              <h4 className="movie__selected--length">
+                <b>Runtime:</b>
+                {movie.runtime}
+              </h4>
+              <div className="summary">
+                <b>Summary</b>
+                <p>
+                  {movie.overview}
+                </p>
+              </div>
             </div>
-        </div>
+          </div>
     </div>
-  )
+  );
 }
 
 export default MovieInfo
