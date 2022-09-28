c2caf74bbf056eaf2209c1285b3b785e6854562b
paul serafim <paul.serafim.3021@gmail.com>
2022-09-28 14:19:58 +1000

    code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,7 +33,7 @@ function Home() {
     <div className="home__container">
         <div className="home__row">
             <div className='home__content'>
-                <h1>Welcome to <span className='grey__text'>OMDb</span> Movie <span className='purple__text'>Databse</span></h1>
+                <h1>Welcome to the <span className='grey__text'>Movie </span><span className='purple__text'>Databse</span></h1>
                 <p className='home__content--para'>Browse through the <span className='yellow__text'>Best</span> Box Office films</p>
             </div>
             <div className='search__box'>
